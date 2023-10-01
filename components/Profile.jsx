4724c1026a5a88f5ec49bd1594f5c75669c7f95e
sofia baezzato <baezzatosofia331gmail.com>
2023-10-01 17:14:04 +0200

    fix cards behavior in profile page

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -10,15 +10,17 @@ const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
       </h1>
       <p className="desc text-left">{desc}</p>
 
-      <div className="mt-10 tool_layout">
-        {data.map((post) => (
-          <ToolCard
-            key={post._id}
-            post={post}
-            handleEdit={() => handleEdit && handleEdit(post)}
-            handleDelete={() => handleDelete && handleDelete(post)}
-          />
-        ))}
+      <div className="feed">
+        <div className="mt-10 tool_layout">
+          {data.map((post) => (
+            <ToolCard
+              key={post._id}
+              post={post}
+              handleEdit={() => handleEdit && handleEdit(post)}
+              handleDelete={() => handleDelete && handleDelete(post)}
+            />
+          ))}
+        </div>
       </div>
     </section>
   )
