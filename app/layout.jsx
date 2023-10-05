c37ac24b1d5632e8d5750c752ead8652ef4faf28
sofia baezzato <baezzatosofia331gmail.com>
2023-10-05 15:37:47 +0200

    move favicon in /app and remove code

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,7 +11,6 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang='en'>
-        <head><link rel="icon" href="./images/favicon.ico" sizes='any'/></head>
         <body>
             <Provider>
                 <div className="main">
