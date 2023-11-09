0435f63abe5448b1f6d7322675c1b63fdde7b027
sofia baezzato <baezzatosofia331gmail.com>
2023-11-09 22:24:01 +0100

    fix cache issuegit

--- next.config.js
+++ next.config.js
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 's-maxage=1, stale-while-revalidate=59',
-          },
-        ],
-      },
-    ];
-  },
-  experimental: {
-    serverComponentsExternalPackages: ["mongoose"],
-  },
   images: {
     remotePatterns: [
       {
