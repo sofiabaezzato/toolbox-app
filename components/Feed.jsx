7863e5cb9ef0ccaf0d0ead853eaa5291ac5f5e81
sofia baezzato <baezzatosofia331gmail.com>
2023-11-10 10:07:34 +0100

    fix cache issuegit

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,8 +2,10 @@
 
 import { useEffect, useState } from 'react'
 import Image from 'next/image'
-
 import ToolCard from './ToolCard'
+
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 const ToolCardList = ({ data, handleTagClick }) => {
   return (
