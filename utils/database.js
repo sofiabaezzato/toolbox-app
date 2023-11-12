a7714db521c2fbdd894177d30d87510b50b0cce5
sofia baezzato <baezzatosofia331gmail.com>
2023-11-13 00:45:53 +0100

    refactor(#29): update mongoose.ConnectOptions

--- utils/database.js
+++ utils/database.js
@@ -1,26 +1,25 @@
 import mongoose from "mongoose";
 
-let isConnected = false
+let dbConnection = null
 
 export const connectToDB = async () => {
-  mongoose.set('strictQuery', true)
-
-  if(isConnected) {
-    console.log('MongoDB is already connected')
-    return
+  if(dbConnection) {
+    return dbConnection
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "add_tool",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    dbConnection = await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "add_tool"
     })
-
-    isConnected = true
-    
     console.log('MongoDB is connected')
+    return dbConnection
+    
   } catch (error) {
     console.log(error)
+    throw new Error('Failed to connect to MOngoDB')
   }
+}
+
+export const isDBConnected = () => {
+  return !!dbConnection
 }
